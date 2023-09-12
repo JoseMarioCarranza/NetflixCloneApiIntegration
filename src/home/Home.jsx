b628e7d731123ced57bf2f57b626593081b2ab8e
JoseMarioCarranza <imt_josecarranza@outlook.com>
2023-09-11 17:28:33 -0700

    code depuration

--- src/home/Home.jsx
+++ src/home/Home.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import NavComponet from '../nav/Nav.jsx';
 import { Outlet } from "react-router-dom";
 import Footer from '../components/footer/Footer.jsx';
-import Movies from '../movies/movies.jsx'
 import './Home.css'
 
 export default function Home() {
